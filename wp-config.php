4085af9efd41c782500ad761fb3c1d8715082f4b
Sasan Sorousheh <sasan.sorousheh@yahoo.com>
2022-02-05 13:43:33 +0330

    change db user

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'new_tico' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'nikuadmin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '@Nikukiosk!' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
